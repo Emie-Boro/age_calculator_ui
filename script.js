3afbbcd3cb8b63a0a190aa8a55de30dbb098f8da
Emie Boro <emiedonmukumo@gmail.com>
2024-01-22 20:19:40 +0100

    Updated files

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
+
 document.getElementById('submit').addEventListener('click', ()=>{
-    // fetch('https://3mtt-age-calculator.cyclic.app/date/2023-12-21')
-    // .then(response => {
-    //     if (!response.ok) {
-    //         throw new Error(`Network response was not ok: ${response.status}`);
-    //     }
-    //     return response.json();
-    //     })
-    // .then(data =>{
-    //     console.log(data)
-    // }).catch(err =>{
-    //     console.log(err)
-    // })
-    alert('Working in Progress...')
+    const year = document.getElementById('year').value;
+    const month = document.getElementById('month').value;
+    const day = document.getElementById('day').value;
+
+    fetch(`https://3mtt-age-calculator.cyclic.app/date/${year}/${month}/${day}`)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(`Network response was not ok: ${response.status}`);
+        }
+        return response.json();
+        })
+    .then(data =>{
+        console.log(data)
+        document.getElementById('ageYear').textContent = data.years
+        document.getElementById('ageMonth').textContent = data.month;
+        document.getElementById('ageDay').textContent = data.days
+    }).catch(err =>{
+        console.log(err)
+    })
 })
+
+    
