0a357da26159823f936ee98da74e33af93e60ea6
Emie Boro <emiedonmukumo@gmail.com>
2024-01-22 15:06:42 +0100

    Adding files

---
+++ script.js
@@ -0,0 +1,14 @@
+document.getElementById('submit').addEventListener('click', ()=>{
+    fetch('https://3mtt-age-calculator.cyclic.app/date/2023-12-21')
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(`Network response was not ok: ${response.status}`);
+        }
+        return response.json();
+        })
+    .then(data =>{
+        console.log(data)
+    }).catch(err =>{
+        console.log(err)
+    })
+})
